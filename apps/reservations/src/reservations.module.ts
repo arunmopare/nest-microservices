8427c0b08a07af3a58c4ddb6b0a42dca96becb32
arunmopare <arunmopare546@gmail.com>
2023-04-22 18:21:02 +0530

    fixed logging and aaded validations

--- apps/reservations/src/reservations.module.ts
+++ apps/reservations/src/reservations.module.ts
@@ -1,21 +1,20 @@
 import { Module } from '@nestjs/common';
 import { ReservationsService } from './reservations.service';
 import { ReservationsController } from './reservations.controller';
-import { DatabaseModule } from '@app/common';
+import { DatabaseModule, LoggerModule } from '@app/common';
 import { ReservationsRepository } from './reservations.repository';
 import {
   ReservationDocument,
   ReservationSchema,
 } from './reservations/models/reservation.schema';
-import { LoggerModule } from 'nestjs-pino';
 
 @Module({
   imports: [
     DatabaseModule,
+    LoggerModule,
     DatabaseModule.forFeature([
       { name: ReservationDocument.name, schema: ReservationSchema },
     ]),
-    LoggerModule.forRoot(),
   ],
   controllers: [ReservationsController],
   providers: [ReservationsService, ReservationsRepository],
