b24417da703fc1734d7a1fc97d99f94a5d8c5fb8
arunmopare <arunmopare546@gmail.com>
2023-04-23 06:41:42 +0530

    added docker for auth microservice, imporved validations

--- apps/auth/src/auth.module.ts
+++ apps/auth/src/auth.module.ts
@@ -2,12 +2,10 @@ import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { UsersModule } from './users/users.module';
-import { ReservationsRepository } from 'apps/reservations/src/reservations.repository';
-import { UsersService } from './users/users.service';
-import { UsersRepository } from './users/users.repository';
+import { LoggerModule } from '@app/common';
 
 @Module({
-  imports: [UsersModule],
+  imports: [UsersModule, LoggerModule],
   controllers: [AuthController],
   providers: [AuthService],
 })
--- apps/reservations/src/main.ts
+++ apps/reservations/src/main.ts
@@ -6,6 +6,6 @@ async function bootstrap() {
   const app = await NestFactory.create(ReservationsModule);
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   app.useLogger(app.get(Logger));
-  await app.listen(3001);
+  await app.listen(3005);
 }
 bootstrap();
